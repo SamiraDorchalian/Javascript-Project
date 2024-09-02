679f4f52d9b7a702fc5e55704960973bc79d95e4
samira dorchalian <samiradorchalain1998@gmail.com>
2024-09-02 13:10:54 +0330

    Project-ToDo

--- project-ToDo/js/script.js
+++ project-ToDo/js/script.js
@@ -1,9 +1,11 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const editButton = document.getElementById("edit-button");
 const alertMessage = document.getElementById("alert-message");
 const deleteAllButton = document.getElementById("delete-all-button");
 const todosBody = document.querySelector("tbody");
+const filterButtons = document.querySelectorAll(".filter-todos");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -30,21 +32,25 @@ const showAlert = (message, type) => {
   }, 2000);
 };
 
-const displayTodos = () => {
+const displayTodos = (data) => {
+  const todoList = data || todos;
   todosBody.innerHTML = "";
-  if (!todos.length) {
+
+  if (!todoList.length) {
     todosBody.innerHTML = "<tr><td colspan='4'>No task found!</td></tr>";
     return;
   }
-  todos.forEach((todo) => {
+  todoList.forEach((todo) => {
     todosBody.innerHTML += `
         <tr>
             <td>${todo.task}</td>
             <td>${todo.date || "No Date"}</td>
             <td>${todo.completed ? "Completed" : "Pending"}</td>
             <td>
-                <button>Edit</button>
-                <button onclick="toggleHandler('${todo.id}')">Do</button>
+                <button onclick="editHandler('${todo.id}')">Edit</button>
+                <button onclick="toggleHandler('${todo.id}')">
+                  ${todo.completed ? "Undo" : "Do"}
+                </button>
                 <button onclick="deleteHandler('${todo.id}')">Delete</button>
             </td>
         </tr>
@@ -92,8 +98,73 @@ const deleteHandler = (id) => {
   showAlert("Todo deleted successfully", "success");
 };
 
-const toggleHandler = (id) => {};
+const toggleHandler = (id) => {
+  // const newTodos = todos.map((todo) => {
+  //   if (todo.id === id) {
+  //     // return {
+  //     //   id: todo.id,
+  //     //   task: todo.task,
+  //     //   date: todo.date,
+  //     //   completed: !todo.completed,
+  //     // };
+  //     return { ...todo, completed: !todo.completed };
+  //   } else {
+  //     return todo;
+  //   }
+  // });
+  // todos = newTodos;
+  const todo = todos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo status change successfully", "success");
+};
 
-window.addEventListener("load", displayTodos);
+const editHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addButton.style.display = "none";
+  editButton.style.display = "inline-block";
+  editButton.dataset.id = id;
+};
+
+const applyEditHAndler = (event) => {
+  const id = event.target.dataset.id;
+  const todo = todos.find((todo) => todo.id === id);
+  todo.task = taskInput.value;
+  todo.date = dateInput.value;
+  taskInput.value = "";
+  dateInput.value = "";
+  addButton.style.display = "inline-block";
+  editButton.style.display = "none";
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo edited successfully", "success");
+};
+
+const filterHandler = (event) => {
+  let filteredTodos = null;
+  const filter = event.target.dataset.filter;
+
+  switch (filter) {
+    case "pending":
+      filteredTodos = todos.filter((todo) => todo.completed === false);
+      break;
+    case "completed":
+      filteredTodos = todos.filter((todo) => todo.completed === true);
+      break;
+    default:
+      filteredTodos = todos;
+      break;
+  }
+  displayTodos(filteredTodos);
+};
+
+window.addEventListener("load", () => displayTodos());
 addButton.addEventListener("click", addHandler);
 deleteAllButton.addEventListener("click", deleteAllHandler);
+editButton.addEventListener("click", applyEditHAndler);
+filterButtons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
