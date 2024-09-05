c32c79d0f6c58f8eafd337ec062f1cc04334afc2
samira dorchalian <samiradorchalain1998@gmail.com>
2024-09-05 16:12:04 +0330

    add file html & css

---
+++ project-QuizApp/js/game.js
@@ -0,0 +1,43 @@
+import formatData from "./helper.js";
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+
+let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
+
+const fetchData = async () => {
+  const respons = await fetch(URL);
+  const json = await respons.json();
+  formattedData = formatData(json.results);
+  start();
+};
+
+const start = () => {
+  showQuestion();
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+const checkAnswer = () => {};
+
+window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
