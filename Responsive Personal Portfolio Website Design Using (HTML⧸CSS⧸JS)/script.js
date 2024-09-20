8356b34c7967b8f4381c00417ce7be8197a5f9a5
samira dorchalian <samiradorchalain1998@gmail.com>
2024-09-20 20:48:30 +0330

    Responsive Personal Portfolio Website Design Using (HTML⧸CSS⧸JS)

--- Responsive Personal Portfolio Website Design Using (HTML⧸CSS⧸JS)/script.js
+++ Responsive Personal Portfolio Website Design Using (HTML⧸CSS⧸JS)/script.js
@@ -39,3 +39,110 @@ if (typed) {
     backDelay: 2000,
   });
 }
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 50;
+
+    let sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".menu a[href *=" + sectionId + "] ")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".menu a[href *=" + sectionId + "] ")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+const pages = document.querySelectorAll(".page");
+const resume = document.querySelector(".resume");
+
+function resumeActive() {
+  const scrollY = window.pageYOffset;
+
+  pages.forEach((page) => {
+    const sectionHeight = page.offsetHeight;
+    const sectionTop = page.offsetTop - 120;
+
+    let sectionId = page.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".resume_tabs a[href *=" + sectionId + "]")
+        .classList.add("current");
+    } else {
+      document
+        .querySelector(".resume_tabs a[href *=" + sectionId + "]")
+        .classList.remove("current");
+    }
+  });
+}
+
+window.addEventListener("scroll", resumeActive);
+
+let filterItems = document.querySelectorAll(".portfolio_filters li");
+
+function activePortfolio() {
+  filterItems.forEach((el) => {
+    el.classList.remove("filter-active");
+    this.classList.add("filter-active");
+  });
+}
+
+filterItems.forEach((el) => {
+  el.addEventListener("click", activePortfolio);
+});
+
+let mixerPortfolio = mixitup(".portfolio_wrap-container", {
+  selectors: {
+    target: ".portfolio_item",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+let swiper = new Swiper(".testimonial_container", {
+  effect: "slide",
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  spaceBetween: 100,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+let modalBtns = document.querySelectorAll(".services_button"),
+  modalViews = document.querySelectorAll(".services_modal"),
+  modalClose = document.querySelectorAll(".modal_close-icon");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((el) => {
+  el.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
