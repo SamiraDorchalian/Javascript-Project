c45d7f94dc1da43c95ed80dd33a54071f4887e30
samira dorchalian <samiradorchalain1998@gmail.com>
2024-09-23 16:58:08 +0330

    add image&file HTML&CSS

--- How to Create Amazing Website by Using (HTML - CSS - JS)/script.js
+++ How to Create Amazing Website by Using (HTML - CSS - JS)/script.js
@@ -15,3 +15,43 @@ function parallax(e) {
     shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
+window.addEventListener("scroll", () => {
+  let about = document.querySelector(".about");
+  if (window.pageYOffset > 317) {
+    about.classList.add("active");
+
+    const counters = document.querySelectorAll(".count");
+    const speed = 1000;
+
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = parseInt(+counter.getAttribute("data-target"));
+        const count = parseInt(+counter.innerText);
+        const increment = Math.trunc(target / speed);
+
+        if (count < target) {
+          counter.innerText = count + increment;
+          setTimeout(updateCount, 100);
+        } else {
+          count.innerText = target;
+        }
+      };
+      updateCount();
+    });
+  }
+});
+
+window.addEventListener("scroll", () => {
+  function getSpaceBetweenTopAndElement() {
+    let elements = document.querySelectorAll(".section-title");
+    elements.forEach((element) => {
+      let rect = element.getBoundingClientRect();
+      let space = rect.y;
+      if (space < 600) {
+        element.style.opacity = "1";
+        element.classList.add("animate__bounceInLeft");
+      }
+    });
+  }
+  getSpaceBetweenTopAndElement();
+});
