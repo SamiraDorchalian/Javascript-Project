d71f5c050f0302e4d2f71f1d0807175cc0b05365
samira dorchalian <samiradorchalain1998@gmail.com>
2024-09-26 15:16:50 +0330

    Add section services

--- How To Create Responsive Website Using (HTML - CSS - JS)/script.js
+++ How To Create Responsive Website Using (HTML - CSS - JS)/script.js
@@ -18,3 +18,72 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+window.addEventListener("scroll", () => {
+  function getSpaceBetweenTopAndElement() {
+    let elements = document.querySelectorAll(".section-title");
+    elements.forEach((element) => {
+      let rect = element.getBoundingClientRect();
+      let space = rect.y;
+      if (space < 600) {
+        element.style.opacity = "1";
+        element.classList.add("animate__bounceInLeft");
+      }
+    });
+  }
+  getSpaceBetweenTopAndElement();
+});
+
+window.addEventListener("scroll", () => {
+  function addClassAnimation() {
+    let elements = document.querySelectorAll(".services-boxs .box");
+    elements.forEach((element) => {
+      let rect = element.getBoundingClientRect();
+      let space = rect.y;
+      if (space < 600) {
+        element.style.opacity = "1";
+        element.classList.add("animate__bounceInUp");
+      }
+    });
+  }
+  addClassAnimation();
+});
+
+window.addEventListener("scroll", () => {
+  function getClassAction() {
+    let elements = document.querySelectorAll("section[id]");
+    elements.forEach((element) => {
+      let rect = element.getBoundingClientRect();
+      let space = rect.y;
+      if (space < 400) {
+        let attId = element.getAttribute("id");
+        let menuItems = document.querySelectorAll(".menu-item");
+        menuItems.forEach((menuItem) => {
+          menuItem.classList.remove("current");
+          if (menuItem.getAttribute("data-id") === attId) {
+            menuItem.classList.add("current");
+          }
+        });
+      }
+    });
+  }
+  getClassAction();
+});
+
+let swiper = new Swiper(".project-container", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
