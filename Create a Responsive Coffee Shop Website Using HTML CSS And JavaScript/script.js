db93838d2ee62b3ca305c7bdd65e41b5022deef9
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-01 16:49:23 +0330

    add file html&css&js && section home&services&review&products&team

---
+++ Create a Responsive Coffee Shop Website Using HTML CSS And JavaScript/script.js
@@ -0,0 +1,12 @@
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
