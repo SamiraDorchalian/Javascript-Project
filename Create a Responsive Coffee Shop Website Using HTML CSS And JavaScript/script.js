3784c2d428102e5844015be00250e6add4f12346
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-02 13:17:51 +0330

    Create a Responsive Coffee Shop Website Using HTML CSS And JavaScript

--- Create a Responsive Coffee Shop Website Using HTML CSS And JavaScript/script.js
+++ Create a Responsive Coffee Shop Website Using HTML CSS And JavaScript/script.js
@@ -10,3 +10,30 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("move");
+  navbar.classList.remove("open-menu");
+};
+
+const animate = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", { origin: "left" });
+animate.reveal(".home-img", { origin: "bottom" });
+animate.reveal(".ser-box, .product-box, .team-box, .book-data", {
+  interval: 100,
+});
