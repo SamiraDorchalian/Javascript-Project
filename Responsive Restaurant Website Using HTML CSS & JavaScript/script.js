a81dd0dec6edfb20f30c3bad1015a833304f59b7
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-03 16:54:05 +0330

    Responsive Restaurant Website Using HTML CSS & JavaScript

--- Responsive Restaurant Website Using HTML CSS & JavaScript/script.js
+++ Responsive Restaurant Website Using HTML CSS & JavaScript/script.js
@@ -1,5 +1,28 @@
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
   menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu");
 };
+
+window.onscroll = () => {
+  menu.classList.remove("move");
+  navbar.classList.remove("open-menu");
+};
+
+const animate = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
+animate.reveal(".home-text", { origin: "left" });
+animate.reveal(".home-img", { origin: "bottom" });
+animate.reveal(".heading, .newsletter h2", { origin: "top" });
+animate.reveal(
+  "header, .features-box,.feature-menu-box,.item-box,.m-item-box,.t-box,.newsletter",
+  {
+    interval: 100,
+  }
+);
