9ccaee27b51e8c7838f64c2782ac26d6fbba0387
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-05 15:55:13 +0330

    create section products & categories

--- Create A Responsive Grocery Store Website Design Using HTML - CSS - JavaScript/script.js
+++ Create A Responsive Grocery Store Website Design Using HTML - CSS - JavaScript/script.js
@@ -39,3 +39,23 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
+
+let swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
