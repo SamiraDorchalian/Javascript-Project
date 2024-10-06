639c4b5f10357cb122e3936d86776be4fddd3b1a
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-06 15:14:12 +0330

    create section review

--- Create A Responsive Grocery Store Website Design Using HTML - CSS - JavaScript/script.js
+++ Create A Responsive Grocery Store Website Design Using HTML - CSS - JavaScript/script.js
@@ -59,3 +59,23 @@ let swiper = new Swiper(".product-slider", {
     },
   },
 });
+
+let swiper2 = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
