1a9d27d3a93f442e438fa384b3f94310c497525c
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-08 17:43:20 +0330

    Complete Responsive Food - Restaurant Website Design Using HTML - CSS -JAVASCRIPT - From Scratch

--- Complete Responsive Food ⧸ Restaurant Website Design Using HTML ⧸ CSS ⧸ JAVASCRIPT - From Scratch/script.js
+++ Complete Responsive Food ⧸ Restaurant Website Design Using HTML ⧸ CSS ⧸ JAVASCRIPT - From Scratch/script.js
@@ -6,9 +6,28 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
 window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
+
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let height = section.offsetHeight;
+    let offset = section.offsetTop - 150;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 };
 
 document.querySelector("#search-icon").onclick = () => {
