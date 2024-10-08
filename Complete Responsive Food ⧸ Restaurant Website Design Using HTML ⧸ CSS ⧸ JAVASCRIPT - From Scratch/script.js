eed77db172917c9a14b23d2fc77e189e3fe50a5b
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-08 15:48:23 +0330

    create section dishes & about & menu & review

--- Complete Responsive Food ⧸ Restaurant Website Design Using HTML ⧸ CSS ⧸ JAVASCRIPT - From Scratch/script.js
+++ Complete Responsive Food ⧸ Restaurant Website Design Using HTML ⧸ CSS ⧸ JAVASCRIPT - From Scratch/script.js
@@ -5,3 +5,54 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
+
+let swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+let swiper2 = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
