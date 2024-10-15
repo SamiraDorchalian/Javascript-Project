f3a825c77caba004de03df801e0cccd5704708e8
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-15 15:57:34 +0330

    add files html's - css - js & create header part

---
+++ Create A Responsive Multipage Furniture Store Website Design  HTML CSS - SASS JavaScript/js/script.js
@@ -0,0 +1,39 @@
+let closer = document.querySelector("#closer");
+
+closer.onclick = () => {
+  closer.style.display = "none";
+  navbar.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  closer.style.display = "block";
+  navbar.classList.toggle("active");
+};
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+  closer.style.display = "block";
+  shoppingCart.classList.toggle("active");
+};
+
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  closer.style.display = "block";
+  loginForm.classList.toggle("active");
+};
+
+let searchForm = document.querySelector(".header .search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+};
