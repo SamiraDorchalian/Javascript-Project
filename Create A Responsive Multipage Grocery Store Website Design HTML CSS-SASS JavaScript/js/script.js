11a567b97fdc9abbb5e491ed33f2deddf21f266f
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-21 17:51:06 +0330

    create section home

--- Create A Responsive Multipage Grocery Store Website Design HTML CSS-SASS JavaScript/js/script.js
+++ Create A Responsive Multipage Grocery Store Website Design HTML CSS-SASS JavaScript/js/script.js
@@ -40,3 +40,18 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
+
+let slides = document.querySelectorAll(".home .slide-container .slide");
+let index = 0;
+
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+function prev() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
